fe8b45e66631f1e45a046deaefc560e2ecf2217b
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-24 10:56:24 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book 
+    attr_accessor :title, :author, :page_count, :genre
+  
+    def initialize(title, author = "Agatha Christie", page_count = 272, genre = "Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+book = Book.new("And Then There Were None")
+
+puts book.author
+puts book.title
+puts book.page_count
+puts book.genre
+
 
 
